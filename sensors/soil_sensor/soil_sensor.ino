95b39785860df5583039846e4241e6f2a7f8a91d
denverpierce <denver.pierce@gmail.com>
2021-07-05 15:44:35 -0500

    Switch to ask sensors api, add deep sleep and blink

--- sensors/soil_sensor/soil_sensor.ino
+++ sensors/soil_sensor/soil_sensor.ino
@@ -1,102 +1,133 @@
-/**
-
-  Soil sensor
-
+/*
+   AskSensors IoT Platform.
+   Description: Soil moisture monitoring using ESP8266 and the AskSensors IoT cloud.
+    Author: https://asksensors.com, 2018 - 2020
+    github: https://github.com/asksensors/AskSensors-ESP8266-Moisture
 */
 
-#include <Arduino.h>
 #include "config.h"
-
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
-
 #include <ESP8266HTTPClient.h>
 
-#include <WiFiClient.h>
+int LED = 2; // Assign LED pin i.e: D1 on NodeMCU
+const char* wifi_ssid = SECRET_SSID;             // SSID
+const char* wifi_password = SECRET_PASS;         // WIFI
+const char* apiKeyIn = SECRET_DEVICE_KEY;      // API KEY IN
+
+// ASKSENSORS config.
+String host = "http://api.asksensors.com";         // ASKSENSORS API host name
 
 ESP8266WiFiMulti WiFiMulti;
 
-int WiFiStrength = 0;
+int moisture_Pin = 0; // Soil Moisture Sensor input at Analog PIN A0
+int moisture_value = 0, moisture_state = 0xFF;
 
-const char* ssid = SECERT_WIFI_SSID;
-const char* password = SECERT_WIFI_PASSWORD;
-const char* serverIp =  SERVER_IP;
 
-// big flag to jiggle some debug params
+// flag to change some debug params
 bool debugMode = false;
 
 void setup() {
   Serial.begin(115200);
-  Serial.setDebugOutput(debugMode);
+  Serial.println("********** Program Start : Soil Moisture monitoring using ESP8266 and AskSensors IoT cloud");
+  Serial.println("Wait for WiFi... ");
 
-  Serial.println();
-  Serial.println();
-  Serial.println();
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW); // turn the LED on
 
-  for (uint8_t t = 4; t > 0; t--) {
-    Serial.printf("[SETUP] WAIT %d...\n", t);
-    Serial.flush();
-    delay(1000);
-  }
-  WiFi.begin(ssid, password);
-
+  Serial.print("********** connecting to WIFI : ");
+  Serial.println(wifi_ssid);
+  WiFi.begin(wifi_ssid, wifi_password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
-  Serial.print("Connected! IP address: ");
+  Serial.println("-> WiFi connected");
+  Serial.println("-> IP address: ");
   Serial.println(WiFi.localIP());
 
-}
-
-int analogValue = 0.0;
-double analogVolts = 0.0;
-
-void loop() {
-    Serial.println("Looping");
-
-    if ((WiFi.status() == WL_CONNECTED)) {
-      analogValue = analogRead(A0); // read the analog signal
-      Serial.printf("Raw Analog is: %d\n", analogValue);
-  
-      WiFiClient client;
-      HTTPClient http;
-  
-      Serial.print("[HTTP] begin...\n");
-      // configure traged server and url
-      String route = (String)"http://" + serverIp  + "/api/mqtt/soil";
-      http.begin(client, route); //HTTP
-      http.addHeader("Content-Type", "application/json");
-  
-      Serial.print("[HTTP] POST...\n");
-      // start connection and send HTTP header and body
-      String payload = (String)"{\"soilAnalog\":" + analogValue  + "}";
-      int httpCode = http.POST(payload);
-  
-      // httpCode will be negative on error
-      if (httpCode > 0) {
-        // HTTP header has been send and Server response header has been handled
-        Serial.printf("[HTTP] POST... code: %d\n", httpCode);
-  
-        // file found at server
-        if (httpCode == HTTP_CODE_OK) {
-          const String& payload = http.getString();
-          Serial.println("received payload:\n<<");
-          Serial.println(payload);
-          Serial.println(">>");
-        }
-      } else {
-        Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
-      }
-  
-      http.end();
-  }
-
+  readAndPostSensorValue();
   if (debugMode) {
-    delay(1000);
+    // 20 seconds
+    Serial.println("Short sleep");
+    digitalWrite(LED, LOW); // turn the LED off
+    ESP.deepSleep(2e7);
   }
   else {
-    delay(300000); // TODO: vastly increase this in production
+    // 30 minute sleep
+    Serial.println("Long sleep");
+    digitalWrite(LED, HIGH); // turn the LED off
+    ESP.deepSleep(1.8e9);
   }
 }
+
+
+void readAndPostSensorValue() {
+  Serial.print("MOISTURE LEVEL : ");
+  moisture_value = analogRead(moisture_Pin);
+  Serial.println(moisture_value);
+
+  // wait for WiFi connection
+  if (WiFi.status() == WL_CONNECTED) {
+
+    WiFiClient client;
+    HTTPClient http;
+
+    Serial.print("[HTTP] begin...\n");
+
+    // Create a URL for the request
+    String url = "";
+    url += host;
+    url += "/write/";
+    url += apiKeyIn;
+    url += "?module1=";
+    url += moisture_value;
+
+    Serial.print("********** requesting URL: ");
+    Serial.println(url);
+    http.begin(client, url); //HTTP
+
+    Serial.println("> Soil moisture level and state were sent to ASKSENSORS");
+
+    Serial.print("[HTTP] GET...\n");
+    // start connection and send HTTP header
+    int httpCode = http.GET();
+
+    // httpCode will be negative on error
+    if (httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+      // file found at server
+      if (httpCode == HTTP_CODE_OK) {
+        blink();
+        String payload = http.getString();
+        Serial.println(payload);
+      }
+    } else {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+
+    http.end();
+
+    Serial.println("********** End");
+  }
+}
+
+void blink() {
+  digitalWrite(LED, HIGH); // LED off
+  delay(500);
+  digitalWrite(LED, LOW); // LED on
+  delay(500);
+  digitalWrite(LED, HIGH); // LED off
+  delay(500);
+  digitalWrite(LED, LOW); // LED on
+  delay(500);
+  digitalWrite(LED, HIGH); // LED off
+  delay(500);
+  digitalWrite(LED, LOW); // LED on
+}
+
+void loop () {}
